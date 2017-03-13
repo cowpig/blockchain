c2d960b149737c14b6cf1389b29c2a4a35963c59
Max <max@recoset.com>
2017-03-12 23:57:41 -0400

    new_block except hash

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,35 @@
-use std::hash::{Hash, Hasher};
-use std::string::ToString;
+use std::hash::{Hash, Hasher, SipHasher};
 
 #[derive(Debug)]
-struct Block<'a> {
-	id: u32,
+struct Block {
+	id: u64,
 	prev_hash: u64,
-	this_hash: u64,
-	data: &'a str,
+	data: String,
 }
 
-impl<'a> Hash for Block<'a> {
+impl Hash for Block {
 	fn hash<H:Hasher>(&self, state:&mut H) {
 		self.id.hash(state);
-		self.this_hash.hash(state);
 		self.prev_hash.hash(state);
 		self.data.hash(state);
 	}
 }
 
+fn new_block(prev_block: Block, data: String) -> Block {
+	return Block {
+		id: prev_block.id + 1,
+		prev_hash: 0, //TODO
+		data: data,
+	}
+}
 
 fn main() {
+	let genesis_block = Block {
+		id: 0,
+		prev_hash: 0,
+		data: "I'm awake!".to_string(),
+	};
 
+	let first_block = new_block(genesis_block, "bojangles".to_string());
+	println!("{:?}", first_block);
 }
