0ba0395dbe660264a819ce66626c22c3d5785ed9
Max <max@recoset.com>
2017-03-17 23:46:33 -0400

    validate block, pick chain

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,28 @@ fn hash<T: Hash>(t: &T) -> u64 {
     return s.finish()
 }
 
-fn next_block<'a>(prev_block: &'a Block, data: String) -> Block {
+fn next_block(prev_block: & Block, data: String) -> Block {
 	return Block {
 		id: prev_block.id + 1,
 		prev_hash: hash(prev_block),
 		data: data,
 	}
+}
+
+fn is_valid_block(prev_block: & Block, new_block: & Block) -> bool {
+	return prev_block.id == new_block.id + 1 && hash(prev_block) == new_block.prev_hash;
+}
+
+fn is_valid_chain(chain: & Vec<Block>) -> bool{
+	// return chain.iter().fold(false, |valid, &b| valid && is_valid());
+	return true
+}
+
+fn resolve<'a>(curr_chain: &'a Vec<Block>, new_chain: &'a Vec<Block>) -> &'a Vec<Block> {
+	if curr_chain.len() < new_chain.len() && is_valid_chain(new_chain) {
+		return new_chain;
+	}
+	return curr_chain;
 }
 
 fn main() {
@@ -37,9 +53,10 @@ fn main() {
 		data: "I'm awake!".to_string(),
 	}];
 
-	let mut next = next_block(blocks.last().unwrap(), "stop. hammertime.".to_string());
+	// this is necessary because rust 
+	let mut next = next_block(blocks.last().unwrap(), "stop.".to_string());
 	blocks.push(next);
-	let mut next = next_block(blocks.last().unwrap(), "stop. hammertime.".to_string());
+	next = next_block(blocks.last().unwrap(), "hammertime.".to_string());
 	blocks.push(next);
 
 	println!("{:?}", blocks);
