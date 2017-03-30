02f52d766261d3d4dc23a92b18ce640d1d296134
Max <max@recoset.com>
2017-03-29 23:41:02 -0400

    impl wordvote

--- src/wordvote.rs
+++ src/wordvote.rs
@@ -29,7 +29,7 @@ impl Vote {
 		return (self.pub_id.clone() + &self.last_hash + &self.nonce).clone();
 	}
 
-	fn is_valid(&self, n_bytes: usize, max_remainder: u8) -> bool {
+	fn is_valid_nonce(&self, n_bytes: usize, max_remainder: u8) -> bool {
 		let mut bytes = hash_bytes(self.concat_string());
 		for byte in bytes[..n_bytes].iter() {
 			if *byte != 0 {
@@ -37,6 +37,10 @@ impl Vote {
 			}
 		}
 		return bytes[n_bytes] < max_remainder;
+	}
+
+	fn get_hash_string(&self) -> String{
+		return hash_string(self.concat_string());
 	}
 }
 
@@ -47,5 +51,23 @@ pub struct WordVote {
 }
 
 impl WordVote {
-
+	fn is_valid(&self, n_bytes: usize, max_remainder: u8) -> bool {
+		if self.votes.len() > 0 {
+			return true
+		}
+		if !self.votes[0].is_valid_nonce(n_bytes, max_remainder) {
+			return false
+		}
+		let mut prev = & self.votes[0];
+		for vote in self.votes[1..].iter() {
+			if prev.get_hash_string() != vote.last_hash {
+				return false
+			}
+			if !vote.is_valid_nonce(n_bytes, max_remainder) {
+				return false
+			}
+			prev = vote;
+		}
+		return true
+	}
 }
